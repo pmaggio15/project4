aa851639e73b11f204e16e7a26637f48fa1b7846
Paige <pmaggio15@gmail.com>
2025-07-05 20:22:00 -0600

    emailjs

---
+++ index.js
@@ -0,0 +1,26 @@
+// template_yvmnzs9
+// service_aou6bn7
+//CI_FwyBFaqAfQM_CS
+
+ function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+
+    emailjs
+        .sendForm (
+            'service_aou6bn7' ,
+            'template_yvmnzs9',
+            event.target,
+            'CI_FwyBFaqAfQM_CS'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList += " modal__overlay--visible"
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on pmaggio15@gmail.com"
+        );
+    })
+}
